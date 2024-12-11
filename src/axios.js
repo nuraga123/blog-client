9f6da25051fa79fab2aa7415e6925c59200a2520
nuraga123 <sabrabat54@gmail.com>
2024-12-11 08:22:56 +0400

    fixs office

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// export const API_URL = "https://blog-server-iyxb.onrender.com";
-export const API_URL = "http://192.168.100.5:4444";
+export const API_URL = "https://blog-server-iyxb.onrender.com";
+// export const API_URL = "http://192.168.100.5:4444";
+// export const API_URL = "http://192.168.0.182:4444";
 
 const instance = axios.create({ baseURL: API_URL });
 
@@ -9,5 +10,3 @@ instance.interceptors.request.use((config) => {
   config.headers.Authorization = localStorage.getItem("token") || "";
   return config;
 });
-
-export default instance;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -14,6 +14,7 @@ import { fetchLogin, selectIsAuth } from '../../redux/slices/auth';
 import { fetchUsers } from '../../redux/slices/users';
 import { useTranslation } from '../../hook/useTranslation';
 import { toast } from 'react-toastify';
+import { CircularProgress } from '@mui/material';
 
 export const Login = () => {
   const { translate } = useTranslation();
@@ -21,6 +22,11 @@ export const Login = () => {
   const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
   const { users } = useSelector((state) => state.users);
+
+  const isUsersLoading = users.status === 'loading';
+
+  console.log('users');
+  console.log(users);
 
   useEffect(() => {
     dispatch(fetchUsers());
@@ -61,78 +67,96 @@ export const Login = () => {
       <Typography classes={{ root: styles.title }} variant="h5">
         {translate('login')}
       </Typography>
-      <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-        <Autocomplete
-          options={emails}
-          getOptionLabel={(option) => (option?.email ? option.email : '')}
-          error={Boolean(errors?.email?.message)}
-          onInputChange={(event, value) => {
-            event.isDefaultPrevented();
-            setValue('email', value);
-            clearErrors('email');
+      {isUsersLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%',
+            padding: '10px 0',
+            margin: '10px 0',
+            border: '2px solid #c4c4c4',
+            borderRadius: 5,
           }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              {...register('email', {
-                required: translate('email_required'),
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: translate('email_invalid'),
-                },
-              })}
-              className={styles.field}
-              label={
-                Boolean(errors?.email?.message)
-                  ? errors?.email?.message
-                  : translate('email')
-              }
-              error={Boolean(errors?.email?.message)}
-              helperText={Boolean(errors?.email?.message)}
-              fullWidth
-            />
-          )}
-        />
-
-        <TextField
-          {...register('password', {
-            required: translate('password_required'),
-            minLength: {
-              value: 8,
-              message: translate('password_min'),
-            },
-          })}
-          className={styles.field}
-          label={translate('password')}
-          error={Boolean(errors.password?.message)}
-          helperText={errors.password?.message}
-          fullWidth
-        />
-        <div>
-          <Button
-            type="submit"
-            size="large"
-            variant="contained"
-            fullWidth
-            disabled={!isValid}
-          >
-            {translate('login')}
-          </Button>
-
-          <Button
-            sx={{
-              padding: 0,
-              margin: '10px 0',
-              fontSize: 16,
-            }}
-            onClick={() => navigate('/support')}
-            variant="contained"
-            fullWidth
-          >
-            {translate('forgot_password')}
-          </Button>
+        >
+          <CircularProgress size={30} />
         </div>
-      </form>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+          <Autocomplete
+            options={emails}
+            getOptionLabel={(option) => (option?.email ? option.email : '')}
+            error={Boolean(errors?.email?.message)}
+            onInputChange={(event, value) => {
+              event.isDefaultPrevented();
+              setValue('email', value);
+              clearErrors('email');
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                {...register('email', {
+                  required: translate('email_required'),
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: translate('email_invalid'),
+                  },
+                })}
+                className={styles.field}
+                label={
+                  Boolean(errors?.email?.message)
+                    ? errors?.email?.message
+                    : translate('email')
+                }
+                error={Boolean(errors?.email?.message)}
+                helperText={Boolean(errors?.email?.message)}
+                fullWidth
+              />
+            )}
+          />
+
+          <TextField
+            {...register('password', {
+              required: translate('password_required'),
+              minLength: {
+                value: 8,
+                message: translate('password_min'),
+              },
+            })}
+            className={styles.field}
+            label={translate('password')}
+            error={Boolean(errors.password?.message)}
+            helperText={errors.password?.message}
+            fullWidth
+          />
+          <div>
+            <Button
+              type="submit"
+              size="large"
+              color="success"
+              variant="contained"
+              disabled={!isValid}
+              fullWidth
+            >
+              {translate('login')}
+            </Button>
+
+            <Button
+              sx={{
+                padding: 1,
+                margin: '10px 0',
+                fontSize: 16,
+                
+              }}
+              onClick={() => navigate('/support')}
+              variant="contained"
+              fullWidth
+            >
+              {translate('forgot_password')}
+            </Button>
+          </div>
+        </form>
+      )}
     </Paper>
   );
 };

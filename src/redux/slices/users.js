a6fbb90865babad1d22bc36a9b18e8c7a30921c4
nuraga123 <sabrabat54@gmail.com>
2024-12-11 13:42:01 +0400

    fixs

--- src/redux/slices/users.js
+++ src/redux/slices/users.js
@@ -1,37 +1,48 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../../axios.js";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../axios.js';
+import { AxiosError } from 'axios';
 
-export const fetchUsers = createAsyncThunk("/users", async () => {
-  const { data } = await api.get("/users");
-  console.log(data);
-  return data;
+export const fetchUsers = createAsyncThunk('/users', async () => {
+  try {
+    const { data } = await api.get('/users');
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    if (error instanceof AxiosError) {
+      return {
+        message: error.response.data.message,
+        user: null,
+      };
+    }
+  }
 });
 
 const initialState = {
   users: {
     items: [],
-    status: "loading",
+    status: 'loading',
   },
 };
 
 const usersSlice = createSlice({
-  name: "users",
+  name: 'users',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchUsers.pending, (state) => {
-        state.users.status = "loading";
+        state.users.status = 'loading';
         state.users.items = [];
       })
       .addCase(fetchUsers.fulfilled, (state, action) => {
         state.users.items = action.payload;
-        state.users.status = "load";
+        state.users.status = 'load';
       })
       .addCase(fetchUsers.rejected, (state) => {
         state.users.items = [];
-        state.users.status = "error";
-      })
+        state.users.status = 'error';
+      });
   },
 });
 
